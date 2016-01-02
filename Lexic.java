d93b90a7020db73cbdb4d8ba0fb79b0e211733a5
Tim Nguyen <nguy1877@umn.edu>
2016-01-02 14:41:11 -0600

    Initial commit

---
+++ Lexic.java
@@ -0,0 +1,17 @@
+/*
+Tim Nguyen
+2016
+Lexic: A Word Game
+*/
+
+public class Lexic {
+    public char[][] board;
+    // board
+    // player hands
+    // remaining tiles
+    // player points
+
+    public Lexic() {
+        board = new char[15][15];
+    }
+}
